7febc968590d6957f2ade858a1342a562aef60e2
kengres <kenguruka24@gmail.com>
2017-12-04 23:25:17 +0300

    custom tags page

--- tag.php
+++ tag.php
@@ -1,69 +1,60 @@
 <?php
 
-get_header();
-$post = get_post($_GET['id']);
+    get_header();
+    $post = get_post($_GET['id']);
 
 ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-
-                <div class="col-md-9">
-<!--<div class="name_tour1">
-<div class="name_tour_text"><?php // the_archive_title( '<h3 class="page-title">', '</h3>' );
-                  
-                    // the_archive_description( '<div class="taxonomy-description">', '</div>' );
-                    ?></div>
-      </div>-->     
-                   
-                    <div class="name_tour">
-            			<div class="name_tour_text"><?php single_tag_title(); ?></div>
-        			</div>       
-                    <?php
-                    if(have_posts()):
-                        while (have_posts()):
-                            the_post(); ?>
-                                <div class="col-md-6">
-                                    <div class="tour_country_wr">
-
-                                        <div class="no-gutter ddd">
-                                            <div class="col-md-12 tour_img">
-
-                                                <div class="tour_country_wr">
-                                                    <a href="<?php the_permalink(); ?>"><?=the_post_thumbnail(array(380,197)); ?></a>
-                                                </div>
-
-
- 
-                                            </div>
-                                            <div class="col-md-6">
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <?php
-                            
-
-
-                        endwhile;
-                    else:
-                        echo '<h3>Результов нет... </h3>';
-                    endif;
-
-                    ?>
-
-
-
-
-
-                </div>
-                <?php
-                get_sidebar();
-                ?>
-            </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-9">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="page-header">
+            <h2 class="text-center text-primary"><?php single_tag_title(); ?></h2>
+          </div> 
         </div>
-    </div>
+        <?php
+          if(have_posts()):
+            while (have_posts()):
+              the_post(); ?>
+
+            <div class="col-xs-12 col-sm-6 tag-tour-col">
+              <a href="<?php the_permalink(); ?>">
+
+                <div class="tag-tour-inner">
+                  <div class="tag-tourImage">
+                    <?php the_post_thumbnail( 'full', array(
+                          'class' => 'img-responsive',
+                          'alt'   => 'Featured Image'
+                      )); ?>
+                  </div>
+                  <div class="tour-inner-mask"></div>
+                  <div class="tourContent">
+                    <div class="tourContent-inner">
+                    </div>
+                  </div>
+                </div>
+              </a>
+                
+            </div>
+
+            <?php
+              endwhile;
+              else:
+                echo '<h3>Результов нет... </h3>';
+            endif;
+
+        ?>
+      
+      </div>
+
+    </div> <!-- col-md-9 -->
+      <?php
+      get_sidebar();
+      ?>
+  </div> <!-- row -->
+</div> <!-- container -->
 <?php
 get_footer();
+?>
