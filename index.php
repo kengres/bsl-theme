bdc0e6625d9b138e33016416bde7c7a7a3c679b1
kengres <kenguruka24@gmail.com>
2017-12-04 23:25:58 +0300

    custom news links

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
                 ));
                 if ($novostiPosts->have_posts()) :
                   while ($novostiPosts->have_posts()) : $novostiPosts->the_post(); ?>
-                    <div class="row">
-                      <div class="col-xs-4 col-sm-4">
+                    <div class="row novostiRow">
+                      <div class="col-xs-4 col-sm-5 col-md-4">
 
                         <a href="<?php the_permalink() ?>" class="thumbnail">
                             <?php the_post_thumbnail('full', array (
@@ -35,11 +35,13 @@
                         </a>
                       </div>
 
-                      <div class="col-xs-6 col-md-8">
-                        <a href="<?php the_permalink() ?>">
-                            <h3><?php the_title() ?></h3>
+                      <div class="col-xs-8 col-sm-7 col-md-8">
+                        <a href="<?php the_permalink() ?>" class="postLink">
+                            <h3 class="noMarginTop xs-small"><?php the_title() ?></h3>
                         </a>
-                        <?php echo get_the_excerpt(); ?>
+                        <div class="hidden-xs">
+                          <?php echo get_the_excerpt(); ?>
+                        </div>
                       </div>
                     </div>
                     
--- templates/home-section3.php
+++ templates/home-section3.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row homeNews">
   <div class="col-xs-12">
     <div class="page-header">
       <h2>Последнние новости</h2>
@@ -14,8 +14,8 @@
   if ($novostiPosts->have_posts()) :
     while ($novostiPosts->have_posts()) : $novostiPosts->the_post(); ?>
     <div class="col-md-12">
-      <div class="row">
-        <div class="col-xs-4 col-sm-4">
+      <div class="row novostiRow">
+        <div class="col-xs-4 col-sm-5 col-md-4">
 
           <a href="<?php the_permalink() ?>" class="thumbnail">
             <?php the_post_thumbnail('full', array (
@@ -26,11 +26,13 @@
 
         </div>
 
-        <div class="col-xs-6 col-md-8">
-          <a href="<?php the_permalink() ?>">
-              <h3><?php the_title() ?></h3>
+        <div class="col-xs-8 col-sm-7 col-md-8">
+          <a href="<?php the_permalink() ?>" class="postLink">
+              <h3 class="noMarginTop xs-small"><?php the_title() ?></h3>
           </a>
-          <?php echo get_the_excerpt(); ?>
+          <div class="hidden-xs">
+            <?php echo get_the_excerpt(); ?>
+          </div>
         </div>
       </div>
     </div>
